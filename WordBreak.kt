11e5d56ba1d6c4aa58f34c5992ba3a1adf171622
NullPoint-A <31205395+NullPoint-A@users.noreply.github.com>
2019-08-02 14:34:40 +0900

    Add files via upload
    WordBreak. Kotlin

---
+++ WordBreak.kt
@@ -0,0 +1,34 @@
+package com.mrpic.chutcha.core.util
+
+import android.text.TextPaint
+
+class WordBreak{
+
+    companion object {
+
+        @JvmStatic
+        fun getWordBreak(span: String, paint: TextPaint, width: Float): String {
+            var value = ""
+            val arr: List<String> = span.split(" ")
+            var line = ""
+            for (a in arr) {
+                val str = line + a
+                val end = paint.breakText(
+                        str, true, width, null)
+                if ((end > 0 && end < str.length)) {
+                    value += (line.replace("\n", "") + "\n")
+                    line = "$a "
+                } else {
+                    line += "$a "
+                }
+
+                if (line.contains("\n")) {
+                    value += line.trim() + "\n"
+                    line = ""
+                }
+            }
+            value += line
+            return value
+        }
+    }
+}
